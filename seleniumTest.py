c140236210f1dc9765b46b43910098c870709587
Tom <justforji_ji@msn.com>
2024-11-28 01:45:48 +0800

    20241128

--- seleniumTest.py
+++ seleniumTest.py
@@ -6,55 +6,70 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 from selenium.webdriver.common.by import By
 
-
 logger = logging.getLogger('mylogger')
-
-def screenshot(driver, reason):
- Screenshot_document = str(datetime.now().year) + str(datetime.now().month) + str(datetime.now().day)
- path = "./screenshot/" + Screenshot_document
- path = os.path.join(path)
- if not os.path.exists(path):
-  os.mkdir(path)
- file_name = reason + "_" + str(datetime.now().hour) +  str(datetime.now().minute) + str(datetime.now().second) + ".png"
- path = os.path.join(path, file_name)
- try:
-    driver.get_screenshot_as_file(path)
-    logger.warning(file_name + 'already be saved success')
- except OSError:
-    logger.warning(file_name + 'failed')
-
 options = webdriver.ChromeOptions()
+
+def screenshot(driver, logger, reason):
+   Screenshot_document = str(datetime.now().year) + str(datetime.now().month) + str(datetime.now().day)
+   path = "./screenshot/" + Screenshot_document
+   path = os.path.join(path)
+   if not os.path.exists(path):
+      os.mkdir(path)
+   file_name = reason + "_" + str(datetime.now().hour) +  str(datetime.now().minute) + str(datetime.now().second) + ".png"
+   path = os.path.join(path, file_name)
+   try:
+      driver.get_screenshot_as_file(path)
+      logger.info(file_name + 'already be saved success')
+   except OSError:
+      logger.error(file_name + 'failed')
+
+def to_homepage(driver):
+   driver.get('https://www.cathaybk.com.tw/cathaybk/')
+   WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located)
+
+def count_creditcard_item_list(driver, logger):
+   driver.find_element(By.CLASS_NAME,"cubre-o-header__burger").click()
+   WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[contains(text(),'產品介紹')]")))
+   driver.find_element(By.XPATH,"//*[contains(text(),'產品介紹')]" ).click()
+   WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[contains(text(),'信用卡')]")))
+   driver.find_element(By.XPATH,"//*[contains(text(),'信用卡')]" ).click()
+   WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"div[class='cubre-o-menuLinkList__content']>a:last-child")))
+   creditcard_count = driver.find_elements(By.CSS_SELECTOR,"div[class='cubre-o-menuLinkList__item is-L2open']>div[class='cubre-o-menuLinkList__content']>a")
+   logger.warning("total creditcard items are " + str(len(creditcard_count)) + "items")
+
+def creditcardlist_to_cardinfo(driver):
+   WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[contains(text(),'卡片介紹')]")))
+   driver.find_element(By.XPATH,"//*[contains(text(),'卡片介紹')]" ).click()
+
+def count_stopcard(driver, logger):
+   WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"section[data-anchor-block='blockname06']"))).click()
+   stopcard_count = driver.find_elements(By.CSS_SELECTOR,"section[data-anchor-block='blockname06']>div>div[class='cubre-o-block__component']>div>div[class='cubre-o-slide__page swiper-pagination-clickable swiper-pagination-bullets']>span")
+   logger.warning("total stopcard are " + str(len(stopcard_count)) + "cards")
+   return len(stopcard_count)
+
+def screenshot_stopcard(driver,logger,stopcard_count):
+   for i in range(stopcard_count):
+      WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"section[data-anchor-block='blockname06']>div>div[class='cubre-o-block__component']>div>div[class='cubre-o-slide__page swiper-pagination-clickable swiper-pagination-bullets']>span:nth-child("+ str(i+1)+")"))).click()
+      WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"section[data-anchor-block='blockname06']>div>div[class='cubre-o-block__component']>div>div[class='swiper-wrapper']>div:nth-child("+ str(i+1)+")"))).click()
+      screenshot(driver,logger,"stopcard")
+      logger.warning( str(i+1) + "th screenshot to be recorded ")
+
 options.add_experimental_option('mobileEmulation',{'deviceName':'Pixel 7'})
 driver = webdriver.Chrome(options=options)
-driver.get('https://www.cathaybk.com.tw/cathaybk/')
-element = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located)
+to_homepage(driver)
+screenshot(driver, logger, "homepage")
+count_creditcard_item_list(driver, logger)
+screenshot(driver, logger, "count creditcard item list")
+creditcardlist_to_cardinfo(driver)
+screenshot_stopcard(driver,logger,count_stopcard(driver,logger))
 
-menu = driver.find_element(By.CLASS_NAME,"cubre-o-header__burger").click()
-element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[contains(text(),'產品介紹')]")))
-product = driver.find_element(By.XPATH,"//*[contains(text(),'產品介紹')]" ).click()
-
-element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[contains(text(),'信用卡')]")))
-creditcard = driver.find_element(By.XPATH,"//*[contains(text(),'信用卡')]" ).click()
-screenshot(driver,"creditcard")
-creditcard_count = driver.find_elements(By.CSS_SELECTOR,"div[class='cubre-o-menuLinkList__item is-L2open']>div[class='cubre-o-menuLinkList__content']>a")
-print("total creditcard")
-print(len(creditcard_count))
-
-logger.warning("total creditcard items are " + str(len(creditcard_count)) + "items")
-
-element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[contains(text(),'卡片介紹')]")))
-cardinfo = driver.find_element(By.XPATH,"//*[contains(text(),'卡片介紹')]" ).click()
-
-element = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located)
-stopcard = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"section[data-anchor-block='blockname06']"))).click()
-
-stopcard_count = driver.find_elements(By.CSS_SELECTOR,"section[data-anchor-block='blockname06']>div>div[class='cubre-o-block__component']>div>div[class='cubre-o-slide__page swiper-pagination-clickable swiper-pagination-bullets']>span")
-print(len(stopcard_count))
-logger.warning("total stopcard are " + str(len(stopcard_count)) + "cards")
-
-for i in range(len(stopcard_count)):
- stopcard = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"section[data-anchor-block='blockname06']>div>div[class='cubre-o-block__component']>div>div[class='cubre-o-slide__page swiper-pagination-clickable swiper-pagination-bullets']>span:nth-child("+ str(i+1)+")"))).click()
- screenshot(driver,"stopcard")
- print( str(i+1) + "th photo")
+options.add_experimental_option('mobileEmulation',{'deviceName':'iPhone 12 Pro'})
+driver = webdriver.Chrome(options=options)
+to_homepage(driver)
+screenshot(driver, logger, "homepage")
+count_creditcard_item_list(driver, logger)
+screenshot(driver, logger, "count creditcard item list")
+creditcardlist_to_cardinfo(driver)
+screenshot_stopcard(driver,logger,count_stopcard(driver,logger))
 
 driver.close()
